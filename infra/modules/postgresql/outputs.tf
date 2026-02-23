d7a0be8527784d1257506d5580ee394fea787ae3
shoten-healthjourneys <shoten@healthjourneys.co.uk>
2026-02-23 11:05:31 +0000

    Getting infrasetup for being able to test

--- infra/modules/postgresql/outputs.tf
+++ infra/modules/postgresql/outputs.tf
@@ -10,6 +10,6 @@ output "database_name" {
 
 output "connection_string" {
   description = "PostgreSQL connection string for asyncpg"
-  value       = "postgresql://${var.admin_username}:${var.admin_password}@${azurerm_postgresql_flexible_server.pg.fqdn}:5432/${azurerm_postgresql_flexible_server_database.db.name}?sslmode=require"
+  value       = "postgresql://${var.admin_username}:${urlencode(var.admin_password)}@${azurerm_postgresql_flexible_server.pg.fqdn}:5432/${azurerm_postgresql_flexible_server_database.db.name}?sslmode=require"
   sensitive   = true
 }
